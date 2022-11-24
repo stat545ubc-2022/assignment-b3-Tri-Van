2ad56707906a074cef60df986c0086813e9f7f97
Minh-Tri Van <vanminhtri95@gmail.com>
2022-11-24 15:26:51 -0800

    Update

--- Tri_BCLiquid_Update/app.R
+++ Tri_BCLiquid_Update/app.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(colourpicker)
 
 
-bcl <- read_csv("C:/Users/Tri Van/Desktop/UBC PhamSci/UBC Y2 FALL 2022/STAT 545A Exploratory Data Analysis/assignment-b3-Tri-Van/Tri_BCLiquid_Update/bcl-data.csv")
+bcl <- read_csv("bcl-data.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -11,7 +11,7 @@ ui <- fluidPage(
   h5("Welcome to my shiny app!"),
   br(),
   #New code
-  checkboxGroupInput("CountryInput","country",
+  checkboxGroupInput("CountryInput","New Feature:Select Country of Origin for your drinks!",
                      choices = c("CANADA","UNITED STATES OF AMERICA","FRANCE","IRELAND","ITALY","BRAZIL", "UNITED KINGDOM", "SPAIN", "GERMANY","PORTUGAL","ARGENTINA","ISRAEL","CZECH REPUBLIC","BELGIUM", "MEXICO","AUSTRALIA","SOUTH AFRICA","CHINA", "CHILE","NETHERLANDS"
                                  ,"JAMAICA","JAPAN","CUBA","GREECE","BULGARIA"
                                  , "DOMINICAN REPUBLIC","NEW ZEALAND","POLAND","AUSTRIA","TRINIDAD AND TOBAGO"
@@ -22,9 +22,8 @@ ui <- fluidPage(
                                  ,"ICELAND","VENEZUELA" ,"CROATIA","RUSSIA","PUERTO RICO"
                                  ,"THAILAND","FRENCH POLYNESIA (TAHITI)","GUATEMALA","PHILIPPINES","MONTENEGRO"
                                  ,"ST. CROIX","PANAMA","VIETNAM","PERU"  ),
-                     selected = NULL), #New code 1
-  colourInput("col", "Choose Color that you can see!", "green"), #New code 3: Choose color for colorblind
-#  textOutput("text"),
+                     selected = NULL), #New Feature 1: (functional widget) This feature is useful as it allows you to filter drink based on Country of origin.
+  colourInput("col", "New Feature: Choose Color that you can see!", "green"), #New Feature 3: (functional widget/UI change) This feature is useful for user to choose the color of the graph that they can see, especially for color blind people.
   #End new code
   sidebarLayout(
     sidebarPanel(
@@ -35,7 +34,7 @@ ui <- fluidPage(
     ),
     mainPanel(
       plotOutput("alcohol_hist"),
-      DT::dataTableOutput("data_table") #New code 2: Create Interactive table
+      DT::dataTableOutput("data_table") #New Feature 2: (UI change) Create Interactive table that user can interact with to filter and reorder their search without changing the graph or other input.
     )
   ),
   a(href="https://github.com/daattali/shiny-server/blob/master/bcl/data/bcl-data.csv", "Link to the original data set")
@@ -47,20 +46,17 @@ server <- function(input, output) {
       bcl %>% filter(Price > input$priceInput[1] &
                        Price < input$priceInput[2] &
                        Type == input$typeInput &
-                       Country == input$CountryInput) #New code 1
+                       Country == input$CountryInput) #New Feature 1: (functional widget)
     })
   output$alcohol_hist <-
     renderPlot({
       filtered_data() %>%
-        ggplot(aes(Alcohol_Content)) + geom_histogram(fill=input$col) #New code 3: Choose color for colorblind
+        ggplot(aes(Alcohol_Content)) + geom_histogram(fill=input$col) #New Feature 3: (functional widget/UI change)
     })
   output$data_table <-
-    DT::renderDataTable({   #New code 2: Create Interactive table
+    DT::renderDataTable({   #New Feature 2: (UI change)
       filtered_data()
     })
-#  output$text <- renderText({
-#      filtered_data()%>%row()%>%as.factor()
-#    })
 }
 # Run the application
 shinyApp(ui = ui, server = server)
